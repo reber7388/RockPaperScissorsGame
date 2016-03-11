123322d9ad73dd75681a12fb39dea59d3d5cbac8
reber7388 <jareber@asu.edu>
2016-03-10 22:36:34 -0700

    Finished the game logic, just need to finish getting everything to display on the screen. Then test to make sure the logic is working correctly.

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -14,10 +14,96 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    enum rockPaperScissors: String {
+        case Rock = "rock"
+        case Paper = "paper"
+        case Scissors = "scissors"
+        case None = "none"
+        
+    }
+    
+    var randomNum = arc4random()%3
+   
+    var checkForWin = false
+    var winScore = 0
+    var loseScore = 0
+    var itsATie = 0
+    var playerChoice: rockPaperScissors = rockPaperScissors.None
+    var compChoice: rockPaperScissors = rockPaperScissors.None
+    
+    @IBOutlet weak var displayWins: UILabel!
+    @IBOutlet weak var displayLoses: UILabel!
+    @IBOutlet weak var displayTie: UILabel!
+    
+   
+    
+    
+    
+    @IBAction func onRockPressed(sender: AnyObject){
+        
+        determinWinner(rockPaperScissors.Rock)
+        
+    }
+    
+    @IBAction func onPaperPressed(sender: AnyObject){
+        
+        determinWinner(rockPaperScissors.Paper)
+        
+    }
+    
+    @IBAction func onScissorsPressed(sender: AnyObject){
+        
+        determinWinner(rockPaperScissors.Scissors)
+        
+    }
+    
+    func determinWinner(win: rockPaperScissors){
+        
+        computerPlayer()
+        
+        //User wins cases
+        if playerChoice == rockPaperScissors.Rock && compChoice == rockPaperScissors.Scissors {
+            checkForWin = true
+            processWin()
+        } else if playerChoice == rockPaperScissors.Paper && compChoice == rockPaperScissors.Rock {
+            checkForWin = true
+            processWin()
+        } else if playerChoice == rockPaperScissors.Scissors && compChoice == rockPaperScissors.Paper {
+            checkForWin = true
+            processWin()
+        } else if   playerChoice == rockPaperScissors.Rock && compChoice == rockPaperScissors.Rock ||
+                    playerChoice == rockPaperScissors.Paper && compChoice == rockPaperScissors.Paper ||
+                    playerChoice == rockPaperScissors.Scissors && compChoice == rockPaperScissors.Scissors {
+            itsATie += 1
+            displayTie.text = "\(itsATie)"
+        }
+        
+    }
+    
+    func computerPlayer(){
+        
+        randomNum = arc4random()%3
+        
+        if randomNum == 0 {
+            compChoice = rockPaperScissors.Rock
+        }else if randomNum == 1 {
+            compChoice = rockPaperScissors.Paper
+        }else if randomNum == 2 {
+            compChoice = rockPaperScissors.Scissors
+        }
+        
+    }
+    
+    func processWin(){
+        if checkForWin == true {
+            winScore += 1
+            displayWins.text = "\(winScore)"
+            
+        } else if checkForWin == false {
+            loseScore += 1
+            displayLoses.text = "\(loseScore)"
+        }
     }
 
 
